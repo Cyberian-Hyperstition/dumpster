c21f73e899ec60b5d8d2d6bce0e318e50a6ca4fa
Cyberian-Hyperstition <123237630+Cyberian-Hyperstition@users.noreply.github.com>
2023-06-11 12:34:48 +0300

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -1,8 +1,7 @@
 import streamlit as st
-st.write("Installing required packages...")
-!pip install vk_api
-!pip install pandas
-st.write("Packages installed.")
+import subprocess
+subprocess.call(['pip', 'install', 'vk_api'])
+
 #импортируем мои модули и time
 import my_modules.memescrapping as memes
 import my_modules.image_comparison as ic
