8d52ebaccd6c6f2d41607bdac082e8a2773b0714
Cyberian-Hyperstition <123237630+Cyberian-Hyperstition@users.noreply.github.com>
2023-06-11 11:14:06 +0300

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -1,7 +1,7 @@
 #импортируем мои модули и time
-import dir.memescrapping as memes
-import dir.image_comparison as ic
-import dir.tg_poster as tg
+from dumpster import memescrapping as memes
+from dumpster import image_comparison as ic
+from dumpster import tg_poster as tg
 import time
 
 #ЗДЕСЬ МОЙ ПОЛЬЗОВАТЕЛЬСКИЙ ТОКЕН ОТ VK API
