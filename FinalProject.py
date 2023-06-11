0370e940204a68bebefa59c5444919d7f07109bd
Cyberian-Hyperstition <123237630+Cyberian-Hyperstition@users.noreply.github.com>
2023-06-11 12:48:06 +0300

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -1,6 +1,4 @@
-import streamlit as st
-import subprocess
-subprocess.check_call(['pip', 'install', 'vk_api'])
+
 #импортируем мои модули и time
 import my_modules.memescrapping as memes
 import my_modules.image_comparison as ic
